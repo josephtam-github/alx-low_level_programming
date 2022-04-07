f0378d3bb028d98743ebb49913f7bdcf63eea33c
Joseph Tam <josephtam247@gmail.com>
2022-04-07 12:25:28 -0700

    Fix code to follow betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  Main Entry Point 
+ *  main - Entry point
  *
- *  Return value 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
