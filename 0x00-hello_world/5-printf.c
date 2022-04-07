b2d5b7aa2c654a4baa73f3f539b0ac00373f33bc
Joseph Tam <josephtam247@gmail.com>
2022-04-07 16:18:19 -0700

    Fix bad syntax in 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("\n" "with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
