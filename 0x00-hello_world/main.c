61e8198484a02f8d976e5d743d6c8ec7a077cd52
Joseph Tam <josephtam247@gmail.com>
2022-04-07 08:36:55 -0700

    Made edits to the main.c file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	printf("Hello World");
-       	return (0);
+
+		printf("Holberton School");
+
+			return (0);
+
 }
