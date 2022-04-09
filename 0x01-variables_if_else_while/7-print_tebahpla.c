b87f72ac9ec3cd39ae3e78480dc5039ad83dbb65
Joseph Tam <josephtam247@gmail.com>
2022-04-08 19:23:32 -0700

    reverse the init and condition

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -8,7 +10,7 @@ int main(void)
 {
 	char c;
 
-	for (c = 'z'; c <= 'a'; --c)
+	for (c = 'z'; c >= 'a'; c--)
 	{
 	putchar(c);
 	}
