435d45675a6065c92bb587e6414a99ddd3116634
Joseph Tam <josephtam247@gmail.com>
2022-04-12 11:45:26 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	char h[] = "_putchar";
+	int m;
+
+	m = 0;
+	while (m <= 8)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loop/README.md
@@ -0,0 +1,1 @@
+The purpose of this lesson is to teach me  C- nested loops and how they work as well as C headers 
